952dde43469dd2e616beb63bc44950b1f8269470
testtt7676 <153448911+testtt7676@users.noreply.github.com>
2025-04-23 20:02:33 +0300

    Add files via upload

---
+++ test4.py
@@ -0,0 +1,22 @@
+# Fake AWS credentials
+aws_access_key_id = "AKIA1234567890FAKE"
+aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+# Fake GitHub token
+GITHUB_TOKEN = "ghp_FAKEgithubTOKEN1234567890example"
+
+# Fake Slack token
+SLACK_TOKEN = "xoxb-123456789012-1234567890123-abcdefghijklmnopqrstuvwx"
+
+# Fake generic password
+db_password = "SuperSecret123!"
+
+# Fake private key
+PRIVATE_KEY = """
+-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDcmhRAfakeprivatekeyEXAMPLE
+-----END PRIVATE KEY-----
+"""
+
+# Fake Stripe API key
+STRIPE_SECRET_KEY = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
