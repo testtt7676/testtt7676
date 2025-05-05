6f0f676b645ad956becf95b075397b505178428f
testtt7676 <153448911+testtt7676@users.noreply.github.com>
2025-05-05 07:15:50 +0300

    Update test7.py

--- test7.py
+++ test7.py
@@ -1,2 +1,2 @@
-hello = postgres://avnadmin:AVNS_cVLjdAfrXhYmPvMr_Q-@pg-256715f8-ytnhy-af8b.k.aivencloud.com:14416/defaultdb?sslmode=require
+hello = postgres://avnadmin:AVNS_oe8Z5rAAq6ZFJo1G0eU@pg-256715f8-ytnhy-af8b.k.aivencloud.com:14416/defaultdb?sslmode=require
 
